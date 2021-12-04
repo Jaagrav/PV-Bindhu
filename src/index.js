a85cf0ab90f09eb6acf2b55820feaa89a0042b3a
jaagrav <jaagravseal03@gmail.com>
2021-12-04 10:58:52 +0530

    Added play next up automatically

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
-// console.log("AutoSkipIntro has loaded successfully!");
+console.log("AutoSkipIntro has loaded successfully!");
 
 setInterval(() => {
   const webplayer = document.getElementById("dv-web-player");
 
   if (window.getComputedStyle(webplayer, null).display !== "none") {
-    // console.log("User is watching some shit...");
+    console.log("User is watching some shit...");
 
     const skipElement = document.querySelector(".skipElement");
     if (skipElement) {
-      // console.log("Skip now!!!!");
+      console.log("Skip now!!!!");
       skipElement.click();
     } else {
-      // console.log("Skip element not found!");
+      console.log("Skip element not found!");
     }
+
+    
+    const nextUp = document.querySelector(".nextUpWrapper .nextUp .nextUpCard");
+    if (nextUp) {
+      console.log("Play Next!!!!");
+      nextUp.click();
+    } else {
+      console.log("Next Element not found");
+    }
+
   } else {
-    // console.log("User is not watching shit...");
+    console.log("User is not watching shit...");
   }
 }, 1000);
